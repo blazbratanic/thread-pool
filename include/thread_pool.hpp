75b071c4a71ada229eebfae91619595cc8540100
Blaz Bratanic <blaz.bratanic@sensum.eu>
2014-08-24 00:54:28 +0200

    Started with thread_pool.

---
+++ include/thread_pool.hpp
@@ -0,0 +1,12 @@
+#ifndef THREAD_POOL_HPP
+#define THREAD_POOL_HPP
+
+
+class thread_pool {
+  template <class SchedulingPolicy>
+  thread_pool(size_t pool_size, SchedulingPolicy scheduling_policy) {
+
+
+};
+
+#endif
